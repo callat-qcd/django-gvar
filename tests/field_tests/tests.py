d89aa12fc26121dca8b176933d42ac6233518c59
Christopher Koerber <software@ckoerber.com>
2020-06-04 20:00:57 +0200

    Added remaining parser tests

--- tests/field_tests/tests.py
+++ tests/field_tests/tests.py
@@ -39,3 +39,17 @@ class GVarParserTestCase(TestCase):
         expected = gvar([1, 2, 3], [2, 3, 4])
         parsed = parse_gvar(expr)
         assert_allclose(expected, parsed)
+
+    def test_03_vector_array(self):
+        """Checks if `[1, 2, 3] | [4, 5, 6]` is converted correctly."""
+        expr = "[1, 2, 3] | [4, 5, 6]"
+        expected = gvar([1, 2, 3], [4, 5, 6])
+        parsed = parse_gvar(expr)
+        assert_allclose(expected, parsed)
+
+    def test_04_cov_array(self):
+        """Checks if `[1, 3, 3] | [[4, 5, 6], [5, 7, 8], ...]` is converted correctly."""
+        expr = "[1, 2, 3] | [[4, 5, 6], [5, 7, 8], [6, 8, 9]]"
+        expected = gvar([1, 2, 3], [[4, 5, 6], [5, 7, 8], [6, 8, 9]])
+        parsed = parse_gvar(expr)
+        assert_allclose(expected, parsed)
