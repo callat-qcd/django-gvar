8ef8d98cb6bec1aeeb3adaa459960b890b77476b
Christopher Koerber <software@ckoerber.com>
2020-06-05 15:51:44 +0200

    Repaired tests

--- tests/field_tests/tests.py
+++ tests/field_tests/tests.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 
 from gvar import gvar
 
-from django_gvar.utils import parse_gvar
+from django_gvar.utils import parse_str_to_gvar
 from django_gvar.testing import assert_allclose
 
 from field_tests.models import ExampleTable
@@ -30,26 +30,26 @@ class GVarParserTestCase(TestCase):
         """Checks if `1(2)` is converted correctly."""
         expr = "1(2)"
         expected = gvar(1, 2)
-        parsed = parse_gvar(expr)
+        parsed = parse_str_to_gvar(expr)
         assert_allclose(expected, parsed)
 
     def test_02_vector_paranthesis(self):
         """Checks if `1(2), 2(3), 3(4)` is converted correctly."""
         expr = "1(2), 2(3), 3(4)"
         expected = gvar([1, 2, 3], [2, 3, 4])
-        parsed = parse_gvar(expr)
+        parsed = parse_str_to_gvar(expr)
         assert_allclose(expected, parsed)
 
     def test_03_vector_array(self):
         """Checks if `[1, 2, 3] | [4, 5, 6]` is converted correctly."""
         expr = "[1, 2, 3] | [4, 5, 6]"
         expected = gvar([1, 2, 3], [4, 5, 6])
-        parsed = parse_gvar(expr)
+        parsed = parse_str_to_gvar(expr)
         assert_allclose(expected, parsed)
 
     def test_04_cov_array(self):
         """Checks if `[1, 3, 3] | [[4, 5, 6], [5, 7, 8], ...]` is converted correctly."""
         expr = "[1, 2, 3] | [[4, 5, 6], [5, 7, 8], [6, 8, 9]]"
         expected = gvar([1, 2, 3], [[4, 5, 6], [5, 7, 8], [6, 8, 9]])
-        parsed = parse_gvar(expr)
+        parsed = parse_str_to_gvar(expr)
         assert_allclose(expected, parsed)
